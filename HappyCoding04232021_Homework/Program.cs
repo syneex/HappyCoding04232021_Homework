f0390932167faa9b1a2787111a4d476d3a9b89a1
Samuel Sülzle <samuel.suelzle@plan-b-gmbh.com>
2021-04-23 11:11:01 +0200

    Add project files.

---
+++ HappyCoding04232021_Homework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace HappyCoding04232021_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> matchedStrings = new List<string>();
+            var input = File.ReadAllText("C:/Users/SamuelS/Downloads/wiki-http-status-codes.txt");
+
+            Regex regex = new Regex(@"\b[0-9]{3}\b");
+            foreach (Match match in regex.Matches(input))
+            {
+                if (!matchedStrings.Contains(match.Value))
+                {
+                    matchedStrings.Add(match.Value);
+                }
+            }
+
+            matchedStrings.Sort();
+            matchedStrings.ForEach(a =>
+            {
+                Console.WriteLine(a);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
